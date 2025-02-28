040393987a60b8dc1f27c7efee0fd3b6ae635733
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-02-28 12:19:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-//complete this code
-class Person {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(age) {
+    this._age = age;
+  }
+}
+
+class Student extends Person {
+  // Method to simulate studying
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  // Method to simulate teaching
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
